3bd2f2c7c0f6c975386a5e2d3abbc0b06bce54da
lalala <364948086@qq.com>
2023-03-12 17:28:50 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="app-view">
+    <div>132</div>
+  </div>
+</template>
+
+<style lang="scss">
+#app-view {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+function _resolve(dir: string) {
+  return path.resolve(__dirname, dir);
+}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': _resolve('src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/styles/scss/main.scss";',
+      },
+    },
+  },
+});
